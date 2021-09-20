23047ad7ce17af82e505cb26d386b5bd382d058a
Pridon Tetradze <pt@debian.fidosdebian.com>
2021-09-20 18:49:48 +0400

    added ejs

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const path = require("path");
 
 const express = require("express");
-const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    defaultLayout: null,
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,8 @@ router.get("/", (req, res, next) => {
     prods: products,
     path: "/",
     hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
 });
 
