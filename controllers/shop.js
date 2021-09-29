ab7c05276ea8d964f0c4b617a1221b69fedf29d5
Pridon Tetradze <pt@debian.fidosdebian.com>
2021-09-29 21:14:38 +0400

    little advancment

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const { purge } = require("../routes/shop");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -13,9 +14,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
-    console.log(product);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
   });
-  res.redirect("/");
 };
 
 exports.getIndex = (req, res, next) => {
